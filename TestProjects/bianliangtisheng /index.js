14889d85b8cb6e616c7a95b764c0685a0ae3dfb0
TomatoGit <chenfanlmm@163.comssh-keygen -t rsa -C chenfanlmm@163.com>
2019-08-16 23:47:37 +0800

    变量提升测试

---
+++ TestProjects/bianliangtisheng /index.js
@@ -0,0 +1,22 @@
+function foo(){
+    console.log(a);
+    // console.log(a);
+    var a=1;
+    // console.log(a);
+    // var a=3;
+    // console.log(a);
+}
+// foo();
+// var a=4;
+foo();
+// console.log(a);
+// // "use strict";
+// console.log(a);
+// const a=2;
+// let a=2;
+// {
+
+//     var a=2;
+// }
+
+//let 和 const可以避免变量提升·
