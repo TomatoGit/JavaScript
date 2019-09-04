15d95a895a18785df7c164c2cb77694ae26ae8e8
TomatoGit <chenfanlmm@163.comssh-keygen -t rsa -C chenfanlmm@163.com>
2019-09-04 10:33:44 +0800

    增加块作用域测试

--- TestProjects/kuaizuoyongyu/kuaizhuoyongyu.js
+++ TestProjects/kuaizuoyongyu/kuaizhuoyongyu.js
@@ -1,11 +1,20 @@
-var foo=true;
-if(foo){
-    var bar = foo*2;
-    console.log(bar);
-}
-bar =bar*2;
-console.log(bar);
+// var foo=true;
+// if(foo){
+//     var bar = foo*2;
+//     console.log(bar);
+// }
+// bar =bar*2;
+// console.log(bar);
 
-//js中true是1，但是不完全等于1
-//if属于全局作用域
-console.log(true===1);
+// //js中true是1，但是不完全等于1
+// //if属于全局作用域
+// console.log(true===1);
+
+
+for(let i=0;i<5;i++){
+    setTimeout(() => {
+        console.log(i);
+    }, i*1000);
+}
+// 结果，每隔一秒分别输出0、1、2、···；  
+// 解释：setTimeout函数是异步函数，for循环是单线程执行的，执行速度快于setTimeout函数，所以相当于循环执行完之后，所有setTimeout函数再同步执行
